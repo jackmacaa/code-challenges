aecb41983c2ba9b3a1c0b503c2df0923cb2779f8
jack.mcloughlin <jackmaca@msn.com>
2022-12-04 00:36:28 +1030

    day 3 p2

--- advent-3/script.mjs
+++ advent-3/script.mjs
@@ -22,9 +22,37 @@ for (const bag of ruckSacks) {
     }
 
 }
-
-let score = 0;
-for (const letter of letters) {
-    score += (prioritys.indexOf(letter) + 1)
+const calculatePriorityScore = (letters) => {
+    let score = 0;
+    for (const letter of letters) {
+        score += (prioritys.indexOf(letter) + 1)
+    }
+    return score
 }
-console.log(score)
+
+console.log(`day 1: ${calculatePriorityScore(letters)}`)
+
+// PART 2
+const input2 = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`
+
+const letters2 = [];
+const ruckSacks2 = data.split('\n')
+
+for(let i = 0; i < ruckSacks2.length; i+=3) {
+
+    for(let j = 0; j < ruckSacks2[i].length; j++) {
+        if(ruckSacks2[i+2]) {
+            if(ruckSacks2[i+1].includes(ruckSacks2[i][j]) && ruckSacks2[i+2].includes(ruckSacks2[i][j])) {
+                letters2.push(ruckSacks2[i][j]);
+                break;
+            }
+        }
+    }
+}
+
+console.log(`day 2: ${calculatePriorityScore(letters2)}`)
