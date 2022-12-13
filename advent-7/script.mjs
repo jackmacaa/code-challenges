a309be605180e67e1899b436e11435b92905d50b
jack.mcloughlin <jackmaca@msn.com>
2022-12-13 11:22:46 +1030

    day 7 p1 updates

--- advent-7/script.mjs
+++ advent-7/script.mjs
@@ -18,7 +18,7 @@ const findSelected = (folders, current) => {
     }
 }
 // PART 1
-const commands = data.split('\n')
+const commands = input.split('\n')
 let folders = []
 let current = '';
 let previous = '';
@@ -99,79 +99,88 @@ for(const cmd of commands) {
     }
 
 }
-//console.log('FOLDERS ' + JSON.stringify(folders))
+//console.log('FOLDERS ' + JSON.stringify(folders[0]))
 
 const calculateFolders = (files) => {
     if(files.child.length === 0){
-        const retVal = [files.isFolder ? 0 : files.size, files.isFolder]
-       //console.log(JSON.stringify(retVal), ',');
+        const retVal = [files.isFolder ? 0 : files.size, files.isFolder, files.name]
+        //console.log(JSON.stringify(retVal), ',');
         return retVal;
     }
 
     const childSizes = files.child.map(child => {
         const value = calculateFolders(child);
-        console.log({value})
+        //console.log(child)
         if(value.includes(true)){
+            if(value[1] < 100000 && value[1] > 2){
+               //console.log(value)
+            }
             return value[1];
         } else {
             return value[0]
         }
         
     });
+    //console.log(childSizes , files.name)
+
     let childTotals = 0;
     for (let i = 0; i < childSizes.length; i++) {
         childTotals += childSizes[i];
     }
+    //console.log(files.isFolder)
 
     const currentSize = files.isFolder ? 0 : files.size
-    const retVal = [currentSize, childTotals, files.isFolder];
-    if(retVal[1] < 100000){
-       // console.log(JSON.stringify(retVal), ',');
-    }
-    
+    const retVal = ['size ' + currentSize, childTotals, files.isFolder, files.name];
+    console.log(JSON.stringify(retVal), ',');
     return retVal;
-  
-
 }
+
+function getFolderSize(item) {
+    let size = item.size;
+    console.log('size '+ item.size)
+    // if(item.isFolder){
+    //     return size
+    // }
+    size += item.child.reduce((acc, i) => {
+        const folderSize = getFolderSize(i)
+        //console.log(folderSize, ',', item)
+        return acc + folderSize
+    },0);
+    //if(size < 100000) console.log(size, ',')
+    
+    return size
+  } 
+
 console.log('[');
-calculateFolders(folders[0])
+getFolderSize(folders[0])
 console.log(']');
 
 const arr = [
-[0,42692,true] ,
-[0,34049,true] ,
-[0,34049,true] ,
-[0,84883,true] ,
-[0,11720,true] ,
-[0,67639,true] ,
-[0,27749,true] ,
-[0,27749,true] ,
-[0,9583,true] ,
-[0,99990,true] ,
-[0,7520,true] ,
-[0,1,true] ,
-[0,1,true] ,
-[0,7521,true] ,
-[0,56573,true] ,
-[0,64573,true] ,
-[0,2,true] ,
-[0,1,true] ,
-[0,1,true] ,
-[0,99770,true] ,
-[0,98160,true] ,
-[0,98160,true] ,
-[0,74360,true] ,
-[0,80203,true] ,
-[0,11300,true] ,
-[0,1,true] ,
-[0,1,true] ,
-[0,4415,true] ,
-[0,1,true] ,
-[0,1,true] ,
-[0,81223,true] ,
-[0,1,true] ,
-[0,1,true] ,
-]  
+    [ 0, 42692, true, 'fpb' ],
+    [ 0, 34049, true, 'spsz' ],
+    [ 0, 34049, true, 'fgbqzm' ],
+    [ 0, 84883, true, 'phf' ],
+    [ 0, 11720, true, 'jsdbnwrc' ],
+    [ 0, 67639, true, 'ssq' ],
+    [ 0, 27749, true, 'nhq' ],
+    [ 0, 27749, true, 'stbczmlq' ],
+    [ 0, 9583, true, 'qzmz' ],
+    [ 0, 99990, true, 'nlwwrz' ],
+    [ 0, 7520, true, 'hbpps' ],
+    [ 0, 7521, true, 'zhmwl' ],
+    [ 0, 56573, true, 'pqm' ],
+    [ 0, 64573, true, 'fhrfrbqg' ],
+    [ 0, 99770, true, 'mvh' ],
+    [ 0, 98160, true, 'ndrcgmd' ],
+    [ 0, 98160, true, 'lfhgjw' ],
+    [ 0, 74360, true, 'dtgnbb' ],
+    [ 0, 80203, true, 'gcq' ],
+    [ 0, 11300, true, 'nts' ],
+    [ 0, 4415, true, 'lngc' ],
+    [ 0, 81223, true, 'qtblb' ],
+    ]
+      
+//const sum = arr.reduce((acc, i) => acc + i, 0)
 let count2 = 0;
 for(let i = 0; i < arr.length; i++){
     if(arr[i][1] > 3){
